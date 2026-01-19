abc7de6d9d9bdfb176bd921cfdedeba9fd64d201
YuHryshchenko <julia.grischenko.2011@gmail.com>
2026-01-19 01:42:44 -0500

    TFDJANGO19 - Updating Creative form styles

--- djangotutorial/creative_app/models.py
+++ djangotutorial/creative_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Comment(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
