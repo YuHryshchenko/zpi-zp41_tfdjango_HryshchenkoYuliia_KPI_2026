f3599c909e1c8cee0a3a1eca1a342f8ffec06b0a
YuHryshchenko <julia.grischenko.2011@gmail.com>
2026-01-19 02:50:52 -0500

    TFDJANGO20 - Updating Image percentages

--- djangotutorial/image_app/views.py
+++ djangotutorial/image_app/views.py
@@ -12,6 +12,7 @@ model = MobileNetV2(weights='imagenet')
 
 def classify_image(request):
     prediction = None
+    prediction_percent = None
     if request.method == 'POST':
         form = MediaForm(request.POST, request.FILES)
         if form.is_valid():
@@ -30,11 +31,16 @@ def classify_image(request):
             # Predict
             preds = model.predict(img_array)
             # Get top 3 predictions: [(class, description, prob), ...]
-            prediction = decode_predictions(preds, top=3)[0] 
+            prediction = decode_predictions(preds, top=3)[0]
+            prediction_percent = [
+                (id, label, prob * 100)
+                for (id, label, prob) in prediction
+            ]
+               
     else:
         form = MediaForm()
 
-    return render(request, 'image_app/index.html', {'form': form, 'prediction': prediction})
+    return render(request, 'image_app/index.html', {'form': form, 'prediction': prediction_percent})
 
 def success(request):
     return HttpResponse('Successfully uploaded!')
